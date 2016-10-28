aaa8197943540d8da67aa1cb09dd1e064145f575
Dave Barker <kzar@kzar.co.uk>
2016-10-28 12:53:09 +0200

    Deal with spam and deleted messages

--- postsync.sh
+++ postsync.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+notmuch tag -spam not folder:Spam
 notmuch tag +spam folder:Spam
+notmuch tag +deleted folder:Trash
+notmuch tag -deleted not folder:Trash
 notmuch tag -inbox folder:Archive OR folder:Sent
 emacsclient -e '(run-hooks `notmuch-postsync-hook)'
--- presync.py
+++ presync.py
@@ -8,14 +8,21 @@ call(["emacsclient", "-e", "(run-hooks 'notmuch-presync-hook)"])
 db = notmuch.Database()
 maildir_path = os.path.realpath(os.path.expanduser("~/Maildir"))
 
+def move_messages(query_string, destination):
+  query = db.create_query(query_string)
+  for message in query.search_messages():
+    old_filename = message.get_filename()
+    path, filename = os.path.split(old_filename)
+    cur_new = path.split(os.sep)[-1]
+    new_filename = os.path.join(maildir_path, destination, cur_new, filename)
+    os.rename(old_filename, new_filename)
+
 # Archive any messages under INBOX*/ which no longer have the inbox tag
 inbox_folders = " OR ".join(["folder:" + path
                              for path in os.listdir(maildir_path)
                              if path.startswith("INBOX")])
-query = db.create_query("NOT tag:inbox AND (" + inbox_folders + ")")
-for message in query.search_messages():
-  old_filename = message.get_filename()
-  path, filename = os.path.split(old_filename)
-  cur_new = path.split(os.sep)[-1]
-  new_filename = os.path.join(maildir_path, "Archive", cur_new, filename)
-  os.rename(old_filename, new_filename)
+move_messages("NOT tag:inbox AND (" + inbox_folders + ")", "Archive")
+
+# Move any junk to the Spam / Trash folders
+move_messages("tag:spam AND NOT folder:Spam", "Spam")
+move_messages("tag:deleted AND NOT folder:Trash", "Trash")
