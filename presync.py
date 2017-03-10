67ac27df76c31dcbcf408d5b508e1fe22f5c37eb
Dave Barker <kzar@kzar.co.uk>
2017-03-10 21:23:56 +0700

    Rename emails when moving them

--- presync.py
+++ presync.py
@@ -1,5 +1,7 @@
 import os
 from subprocess import call
+import time
+import uuid
 
 import notmuch
 
@@ -11,6 +13,20 @@ db = notmuch.Database()
 maildir_path = os.path.realpath(os.path.expanduser("~/Maildir"))
 
 previous_queries = []
+
+def new_name(old_name):
+  # Most of my email files are named like suggested here[1]. For those we can
+  # replace the timestamp part with the current time.
+  # [1] - http://cr.yp.to/proto/maildir.html
+  first_dot = old_name.find(".")
+  if first_dot > -1:
+    return str(int(time.time())) + old_name[first_dot:]
+
+  # Otherwise we just use a UUID, preserving the flag part if present.
+  first_colon = old_name.find(":")
+  if first_colon > -1:
+    return str(uuid.uuid1()) + old_name[first_colon:]
+  return str(uuid.uuid1())
 
 def move_messages(query_string, destination):
   # Avoid trying to move the same messages multiple times
@@ -24,7 +40,11 @@ def move_messages(query_string, destination):
     old_filename = message.get_filename()
     path, filename = os.path.split(old_filename)
     cur_new = path.split(os.sep)[-1]
-    new_filename = os.path.join(maildir_path, destination, cur_new, filename)
+    new_filename = os.path.join(maildir_path, destination,
+                                cur_new, new_name(filename))
+    # Note this might fail if the filename already exists. That's extremely
+    # unlikely however, and the file will be renamed next time this script runs
+    # anyway.
     os.rename(old_filename, new_filename)
 
 # Move any junk to the Spam / Trash folders
