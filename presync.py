0a0c191ad2ea0b12f75b1dc710727c9adafac550
Dave Barker <kzar@kzar.co.uk>
2017-03-10 10:48:12 +0700

    Avoid trying to move the same messages multiple times

--- presync.py
+++ presync.py
@@ -10,7 +10,15 @@ with open(os.devnull, "w") as devnull:
 db = notmuch.Database()
 maildir_path = os.path.realpath(os.path.expanduser("~/Maildir"))
 
+previous_queries = []
+
 def move_messages(query_string, destination):
+  # Avoid trying to move the same messages multiple times
+  previously_moved = " OR ".join(previous_queries)
+  previous_queries.append("(" + query_string + ")")
+  if previously_moved:
+    query_string += " AND NOT (" + previously_moved + ")"
+  # Move them
   query = db.create_query(query_string)
   for message in query.search_messages():
     old_filename = message.get_filename()
@@ -19,9 +27,9 @@ def move_messages(query_string, destination):
     new_filename = os.path.join(maildir_path, destination, cur_new, filename)
     os.rename(old_filename, new_filename)
 
-# Archive any messages in INBOX which no longer have the inbox tag
-move_messages("NOT tag:inbox AND folder:INBOX", "Archive")
-
 # Move any junk to the Spam / Trash folders
 move_messages("tag:spam AND NOT folder:Spam", "Spam")
 move_messages("tag:deleted AND NOT folder:Trash", "Trash")
+
+# Archive any messages in INBOX which no longer have the inbox tag
+move_messages("NOT tag:inbox AND folder:INBOX", "Archive")
